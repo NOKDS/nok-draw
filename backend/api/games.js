a9d6c1b7c5f350ca2ddb178866f891200b61be82
naa-7 <nalawad000@citymail.cuny.edu>
2023-11-19 19:53:41 -0500

    refractor[routes]: split the routes into two categories

--- backend/api/games.js
+++ backend/api/games.js
@@ -1,12 +1,34 @@
 const router = require("express").Router();
 const { Game } = require("../db/models");
+const { isAuthenticated } = require("../middleware/authMiddleware");
 
-router.get("/", async (req, res, next) => {
+router.get("/", isAuthenticated, async (req, res, next) => {
   try {
-    const allGames = await Game.findAll();
-    allGames
-      ? res.status(200).json(allGames)
-      : res.status(404).send("Games Listing Not Found");
+    console.log(req.user.id);
+    const userId = req.user.id;
+    const games = await Game.findAll({
+      where: { UserId: userId },
+    });
+
+    res.status(200).json(games);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", isAuthenticated, async (req, res, next) => {
+  try {
+    const gameData = req.body;
+    gameData.UserId = req.user.id;
+    const game = await Game.create(gameData);
+
+    if (game) {
+      console.log("Game added successfully");
+      res.status(200).json({ message: "Game added successfully" });
+    } else {
+      console.log("Failed to create game");
+      res.status(400).json({ error: "Failed to create game" });
+    }
   } catch (error) {
     next(error);
   }
--- backend/api/user.js
+++ backend/api/user.js
@@ -41,35 +41,4 @@ router.put("/", isAuthenticated, async (req, res, next) => {
   }
 });
 
-router.post("/", isAuthenticated, async (req, res, next) => {
-  try {
-    const gameData = req.body;
-    gameData.UserId = req.user.id;
-    const game = await Game.create(gameData);
-
-    if (game) {
-      console.log("Game added successfully");
-      res.status(200).json({ message: "Game added successfully" });
-    } else {
-      console.log("Failed to create game");
-      res.status(400).json({ error: "Failed to create game" });
-    }
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.get("/games", isAuthenticated, async (req, res, next) => {
-  try {
-    const userId = req.user.id;
-    const games = await Game.findAll({
-      where: { UserId: userId },
-    });
-
-    res.status(200).json(games);
-  } catch (error) {
-    next(error);
-  }
-});
-
 module.exports = router;
