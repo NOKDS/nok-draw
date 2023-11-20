f0e23d8ec92d53f425c094c7f08b68a2b941057b
naa-7 <nalawad000@citymail.cuny.edu>
2023-11-19 19:54:28 -0500

    tweak[seed]: add some tweaks after adding features to games model

--- backend/seed.js
+++ backend/seed.js
@@ -14,9 +14,14 @@ const generateFakeUser = async () => {
   };
 };
 
-const generateFakeGame = (user) => {
+const generateFakeGame = async (user) => {
+  const isWon = await faker.datatype.boolean();
+
   return {
-    isWon: faker.datatype.boolean(),
+    isWon,
+    top3Predications: ["Dolphin", "Shark", "Bird"],
+    image:
+      "https://i.pinimg.com/originals/2d/98/56/2d9856de5a0d3819b0b84d7b3333d0a5.jpg",
     UserId: user.id,
   };
 };
@@ -27,17 +32,18 @@ const seedData = async () => {
     const userData = await Promise.all(
       Array.from({ length: 10 }, generateFakeUser)
     );
-    console.log("Fake user data:", userData);
     const userDataJSON = JSON.stringify(userData, null, 2);
     fs.writeFileSync("fakeUserData.json", userDataJSON, "utf-8");
 
     const users = await User.bulkCreate(userData);
 
     const games = [];
-    users.forEach((user) => {
-      const userGames = Array.from({ length: 5 }, () => generateFakeGame(user));
-      games.push(...userGames);
-    });
+    await Promise.all(
+      users.map(async (user) => {
+        const userGame = await generateFakeGame(user);
+        games.push(userGame);
+      })
+    );
 
     await Game.bulkCreate(games);
 
