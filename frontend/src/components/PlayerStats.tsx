b7fdbe64e614f6d039b1b2e19782046d4de81e54
naa-7 <nalawad000@citymail.cuny.edu>
2023-11-23 23:33:20 -0500

    tweak[component]: overhaul the component and break it into two components

--- frontend/src/components/PlayerStats.tsx
+++ frontend/src/components/PlayerStats.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Paper, Typography } from "@mui/material";
+import SentimentVerySatisfiedIcon from "@mui/icons-material/SentimentVerySatisfied";
+import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
+import { useTheme } from "../context/ThemeContext";
 
 interface Game {
   id: number;
   isWon: boolean;
   top3Predications: string;
-  image: string;
   createdAt: string;
 }
 
@@ -13,6 +16,8 @@ interface PlayerStatsProps {
 }
 
 const PlayerStats: React.FC<PlayerStatsProps> = ({ games }) => {
+  const { darkMode } = useTheme();
+
   const stats = games.reduce(
     (acc, game) => {
       if (game.isWon) {
@@ -26,12 +31,62 @@ const PlayerStats: React.FC<PlayerStatsProps> = ({ games }) => {
   );
 
   return (
-    <div>
-      <h3>Player Stats</h3>
-      <p>Games Played: {games.length}</p>
-      <p>Wins: {stats.wins}</p>
-      <p>Losses: {stats.losses}</p>
-    </div>
+    <>
+      <Paper
+        sx={{
+          p: 2,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          borderRadius: "1rem",
+          boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+          backgroundColor: darkMode ? "inherit" : "beige",
+        }}
+      >
+        <Typography variant="h5" sx={{ color: "#007BFF", marginBottom: 2 }}>
+          Player Stats
+        </Typography>
+        <Typography
+          variant="body1"
+          sx={{ fontSize: "1.1rem", marginBottom: 1 }}
+        >
+          Games Played: {games.length}
+        </Typography>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            width: "100%",
+            marginBottom: 1,
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              color: "green",
+            }}
+          >
+            <SentimentVerySatisfiedIcon sx={{ marginRight: 1 }} />
+            <Typography variant="body1" sx={{ fontSize: "18px" }}>
+              Wins: {stats.wins}
+            </Typography>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              color: "red",
+            }}
+          >
+            <SentimentVeryDissatisfiedIcon sx={{ marginRight: 1 }} />
+            <Typography variant="body1" sx={{ fontSize: "18px" }}>
+              Losses: {stats.losses}
+            </Typography>
+          </div>
+        </div>
+      </Paper>
+    </>
   );
 };
 
