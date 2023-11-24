3f2d122f4fff2161a5e8ef284b62eeb81f2ec328
naa-7 <nalawad000@citymail.cuny.edu>
2023-11-23 23:31:32 -0500

    feature[category]: add category column to model

--- backend/db/models/game.js
+++ backend/db/models/game.js
@@ -6,12 +6,16 @@ const Game = db.define("Game", {
     type: DataTypes.BOOLEAN,
     allowNull: false,
   },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
   top3Predications: {
     type: DataTypes.STRING,
     allowNull: false,
   },
   image: {
-    type: DataTypes.STRING,
+    type: DataTypes.TEXT,
     allowNull: false,
   },
 });
