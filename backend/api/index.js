adbb8920a202fed10e55b0b33cf81fc8860fca31
naa-7 <nalawad000@citymail.cuny.edu>
2023-11-23 23:30:51 -0500

    feature[new-route]: add new route to index file

--- backend/api/index.js
+++ backend/api/index.js
@@ -4,6 +4,7 @@ router.use("/user", require("./user"));
 router.use("/games", require("./games"));
 router.use("/me", require("./user"));
 router.use("/test", require("./test"));
+router.use("/predict", require("./predictDrawing"));
 
 router.use((req, res, next) => {
   const error = new Error("404 Not Found");
