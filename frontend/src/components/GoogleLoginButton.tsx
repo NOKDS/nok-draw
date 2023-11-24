843ad426cb702e6ac53efc2ccc0eab42bb2f5c19
naa-7 <nalawad000@citymail.cuny.edu>
2023-11-24 09:47:40 -0500

    update

--- frontend/src/components/GoogleLoginButton.tsx
+++ frontend/src/components/GoogleLoginButton.tsx
@@ -15,7 +15,7 @@ const GoogleLoginButton: React.FC = () => {
   const { darkMode } = useTheme();
 
   const handleGoogleSignIn = () => {
-    window.location.href = "https://nok-draw-backend.vercel.app/auth/google/";
+    window.location.href = "https://nok-draw-backend.vercel.app/auth/google";
   };
 
   useEffect(() => {
