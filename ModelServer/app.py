98e7628903b62ccec4bdf4814c77fe88a9dbe8f2
naa-7 <nalawad000@citymail.cuny.edu>
2023-11-23 23:29:49 -0500

    fix[server]: fix cors problem and refractor the code

--- ModelServer/app.py
+++ ModelServer/app.py
@@ -15,42 +15,50 @@ categories = ["bear", "bird", "cat", "cow", "dog", "elephant", "horse", "lion", 
 model = load_model('NOK_CNN.h5', compile=False)
 
 def preprocess_canvas_image(base64_str, target_size=(28, 28)):
-    # Decode the base64 string
-    base64_data = base64_str.split(",")[-1]
-    image_data = base64.b64decode(base64_data)
-    image = Image.open(io.BytesIO(image_data))
+    try:
+        # Decode the base64 string
+        base64_data = base64_str.split(",")[-1]
+        image_data = base64.b64decode(base64_data)
+        image = Image.open(io.BytesIO(image_data))
 
-    # Convert transparency to white if necessary
-    img = convert_transparency_to_white(image)
-    img = np.array(img)
+        # Convert transparency to white if necessary
+        img = convert_transparency_to_white(image)
+        img = np.array(img)
 
-    # Resize the image
-    resized_img = cv2.resize(img, target_size, interpolation=cv2.INTER_AREA)
+        # Resize the image
+        resized_img = cv2.resize(img, target_size, interpolation=cv2.INTER_AREA)
 
-    # Convert to grayscale
-    gray_img = cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY)
+        # Convert to grayscale
+        gray_img = cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY)
 
-    # If inverting colors is not necessary, comment the next line out
-    inverted_img = cv2.bitwise_not(gray_img)
+        # If inverting colors is not necessary, comment the next line out
+        inverted_img = cv2.bitwise_not(gray_img)
 
-    _, thresholded_img = cv2.threshold(inverted_img, 0, 255, cv2.THRESH_BINARY)
-    # Normalize pixel values to the range [0, 1]
-    normalized_img = thresholded_img / 255.0
+        _, thresholded_img = cv2.threshold(inverted_img, 0, 255, cv2.THRESH_BINARY)
+        # Normalize pixel values to the range [0, 1]
+        normalized_img = thresholded_img / 255.0
 
-    # Reshape the normalized image to a 3D array
-    reshaped_img = normalized_img.reshape((28, 28, 1))
+        # Reshape the normalized image to a 3D array
+        reshaped_img = normalized_img.reshape((28, 28, 1))
 
-    return reshaped_img
+        return reshaped_img
+    except Exception as e:
+            print(f"Error during image preprocessing: {e}")
 
 def convert_transparency_to_white(image):
-    if image.mode in ('RGBA', 'LA') or (image.mode == 'P' and 'transparency' in image.info):
-        # Using a white background
-        alpha = image.convert('RGBA').split()[-1]
-        bg = Image.new("RGB", image.size, (255, 255, 255))
-        bg.paste(image, mask=alpha)
-        return bg
-    else:
-        return image
+    try:
+        if image.mode in ('RGBA', 'LA') or (image.mode == 'P' and 'transparency' in image.info):
+            image = image.convert('RGBA')
+            alpha = image.split()[-1]
+
+            bg = Image.new("RGB", image.size, (255, 255, 255))
+            bg.paste(image, mask=alpha)
+            return bg
+        else:
+            return image
+    except Exception as e:
+        print(f"Error during transparency conversion: {e}")
+
 
 @app.route('/predict', methods=['POST'])
 @cross_origin()
