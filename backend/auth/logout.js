0f152ccabdb8b4e37c5386448a738d19ab7ce99a
naa-7 <nalawad000@citymail.cuny.edu>
2023-11-24 20:30:56 -0500

    update logic a little bit

--- backend/auth/logout.js
+++ backend/auth/logout.js
@@ -1,11 +1,15 @@
 const router = require("express").Router();
 
 router.get("/", (req, res, next) => {
-  const user = req.user.dataValues.username;
+  if (!req.isAuthenticated()) {
+    return res.status(401).json({ error: "Not authenticated" });
+  }
+  // const user = req.user.dataValues.username;
   req.logout((error) => {
     if (error) {
       return next(error);
     }
+
     req.session.destroy((error) => {
       if (error) {
         console.error("Error occurred during logout:", error);
@@ -13,6 +17,7 @@ router.get("/", (req, res, next) => {
           .status(500)
           .json({ error: "An error occurred during logout" });
       }
+
       console.log(`${user} logged out successfully`);
       res.status(200).json({ message: "Logged out successfully" });
     });
